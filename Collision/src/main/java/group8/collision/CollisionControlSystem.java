07896d68a04a3f25f2e5ad16ce1af08e21c217bf
Matias Marek <matias_marek@icloud.com>
2018-04-19 15:10:39 +0200

    Added CommonColission and startet specifying services IMovingCollision and IDamageCollision The development is inspired by asteroidsplit in osgi

--- Collision/src/main/java/group8/collision/CollisionControlSystem.java
+++ Collision/src/main/java/group8/collision/CollisionControlSystem.java
@@ -1,106 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package group8.collision;
-
-import group8.common.data.Entity;
-import group8.common.data.GameData;
-import group8.common.data.World;
-import group8.common.services.IEntityProcessingService;
-import org.openide.util.lookup.ServiceProvider;
-import org.openide.util.lookup.ServiceProviders;
-import java.awt.Rectangle;
-
-/**
- * @author group8
- */
-@ServiceProviders(value = {
-    @ServiceProvider(service = IEntityProcessingService.class)
-})
-
-public class CollisionControlSystem implements IEntityProcessingService {
-
-    
-
-    @Override
-    public void process(GameData gameData, World world) {
-
-    }
-
-    /**
-     * This method calculates a rectangle based on a entity and returns it. 
-     * @param entity
-     * @return entityRectangle  
-     */
-    private Rectangle getEntityRect(Entity entity) {
-        //Test to see how much info we loose when converting to int. 
-        int x = (int) (entity.getShapeX()[0]); 
-        int y = (int) (entity.getShapeY()[0]);
-        int width = (int) (entity.getShapeX()[3] - x);
-        int height = (int) (entity.getShapeY()[1] - y);
-        return new Rectangle(x, y, width, height);  
-    }
-    
-    /**
-     * Checks if two entity are of the same class. 
-     * @param entity1 
-     * @param entity2
-     * @return true if entity is the same / false if entity is not the same
-     */
-    private static boolean isSameEntity(Entity entity1, Entity entity2) {
-        return entity1.getID().equals(entity2.getID());
-        //Perhaps redundant. 
-    }
-    /**
-     * Matias: 
-     * hvis vi kigger om collision for alle Zombier først, 
-     * Hvis vi kigger om collision for alle Player 
-     * Altså måske se på alle moving entity om der er collision
-     * reager på den collisoin
-     * måske gøre således at vi sender et collision objekt der indeholder relevante objekter
-     *  - eller vil dette beskade vores kobling. 
-     */
-    private void checkCollision(Entity entity, World world) {
-        //entityRectangle is a rectangle based on the enyity that moves. 
-        Rectangle movingEntityRectangle = this.getEntityRect(entity); 
-        
-        //Rectangle rectangle = this.getEntityRect(entity);
-        //Checks for all entity if they have made a collision with 
-       // Collection correctedCollection = world.getEntities();
-      //  correctedCollection.removeAll(world.getEntities(entity.getClass())); 
-        for(Entity somekindOfEntity :  world.getEntities()){
-            //If we are not looking at collision between the same object
-            if(isSameEntity(somekindOfEntity, entity)){
-             Rectangle someKindofEntityRectangle = getEntityRect(somekindOfEntity);
-             
-            }
-        }
-        
-//        for (Entity e : world.getEntities()) {
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package group8.collision;
 //
-//            if (!this.isSameEntity(e, entity)) {
-//                float[] entity2Rect = this.getEntityRect(e);
+//import group8.common.data.Entity;
+//import group8.common.data.GameData;
+//import group8.common.data.World;
+//import group8.common.services.IEntityProcessingService;
+//import org.openide.util.lookup.ServiceProvider;
+//import org.openide.util.lookup.ServiceProviders;
+//import java.awt.Rectangle;
 //
-//                if (entity1Rect[0] < entity2Rect[0] + entity2Rect[2]
-//                        && entity1Rect[0] + entity1Rect[2] > entity2Rect[0]
-//                        && entity1Rect[1] < entity2Rect[1] + entity2Rect[3]
-//                        && entity1Rect[3] + entity1Rect[1] > entity2Rect[1]) {
-//                    this.setCollisionDir(entity, e, entity1Rect, entity2Rect, dir);
-//                    System.out.println("-----------------------------------------");
+///**
+// * @author group8
+// */
+//@ServiceProviders(value = {
+//    @ServiceProvider(service = IEntityProcessingService.class)
+//})
+//
+//public class CollisionControlSystem implements IEntityProcessingService {
+//
+//    
+//
+//    @Override
+//    public void process(GameData gameData, World world) {
+//
+//    }
+//
+//    /**
+//     * This method calculates a rectangle based on a entity and returns it. 
+//     * @param entity
+//     * @return entityRectangle  
+//     */
+//    private Rectangle getEntityRect(Entity entity) {
+//        //Test to see how much info we loose when converting to int. 
+//        int x = (int) (entity.getShapeX()[0]); 
+//        int y = (int) (entity.getShapeY()[0]);
+//        int width = (int) (entity.getShapeX()[3] - x);
+//        int height = (int) (entity.getShapeY()[1] - y);
+//        return new Rectangle(x, y, width, height);  
+//    }
+//    
+//    /**
+//     * Checks if two entity are of the same class. 
+//     * @param entity1 
+//     * @param entity2
+//     * @return true if entity is the same / false if entity is not the same
+//     */
+//    private static boolean isSameEntity(Entity entity1, Entity entity2) {
+//        return entity1.getID().equals(entity2.getID());
+//        //Perhaps redundant. 
+//    }
+//    
+//    /**
+//     * Note : Det er denne service som dette komponent kan tilbyde.
+//     * This method checks for collision between 2 different entity types.
+//     * @param entity 
+//     * @param world
+//     * @return true if there is a collision / false if same type or no collision. 
+//     */
+//    private boolean checkCollision(Entity entity, World world) {
+//        System.out.println("Check Collision");
+//        //entityRectangle is a rectangle based on the enyity.
+//        Rectangle movingEntityRectangle = this.getEntityRect(entity); 
+//        //Look for collesion between all entity in the world.
+//        for(Entity somekindOfEntity :  world.getEntities()){
+//            //make sure we do not check on the identical entity.
+//            if(!isSameEntity(somekindOfEntity, entity)){
+//                //Draw the rectangle for any given entity that is not the "original" entity. 
+//             Rectangle someKindofEntityRectangle = getEntityRect(somekindOfEntity);
+//                //Get the collisionRectangle. 
+//                Rectangle collisionRectangle = rectangleIntersection(movingEntityRectangle, someKindofEntityRectangle);
+//                //collisionRectanlge is null when there is no coliison
+//                if(collisionRectangle == null){
+//                    return false; //Collision not happening
+//                }
+//                //there is a collision rectangle and therefor collision. 
+//                else{
+//                    return true; //Collision between 2 entitys.
 //                }
 //            }
 //        }
-    }
-    /**
-     * Returns a new rectangle equal to a rectangle between two collided rectangles
-     * @param rectangle1
-     * @param rectangle2
-     * @return rectangle     
-     */
-    public Rectangle rectangleIntersection(Rectangle rectangle1, Rectangle rectangle2){
-        return rectangle1.intersection(rectangle2);
-    }
-
-}
+//    return false; 
+//    }
+//    /**
+//     * Returns a new rectangle equal to a rectangle between two collided rectangles
+//     * @param rectangle1
+//     * @param rectangle2
+//     * @return rectangle     
+//     */
+//    public Rectangle rectangleIntersection(Rectangle rectangle1, Rectangle rectangle2){
+//        return rectangle1.intersection(rectangle2);
+//    }
+//
+//}
--- Collision/src/main/java/group8/collision/CollisionPluginSystem.java
+++ Collision/src/main/java/group8/collision/CollisionPluginSystem.java
@@ -1,37 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package group8.collision;
-
-import group8.common.data.Entity;
-import group8.common.data.GameData;
-import group8.common.data.World;
-import group8.common.mapcommon.IMapCollision;
-import group8.common.services.IGamePluginService;
-import java.util.ArrayList;
-import org.openide.util.Lookup;
-import org.openide.util.lookup.ServiceProvider;
-import org.openide.util.lookup.ServiceProviders;
-
-/**
- *
- * @author jacob
- */
-@ServiceProviders(value = {@ServiceProvider(service = IGamePluginService.class)})
-public class CollisionPluginSystem implements IGamePluginService  {
-    
-    
-    @Override
-    public void start(GameData gameData, World world) {
-       
-        
-    }
-    
-    @Override
-    public void stop(GameData gameData, World world) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package group8.collision;
+//
+//import group8.common.data.Entity;
+//import group8.common.data.GameData;
+//import group8.common.data.World;
+//import group8.common.mapcommon.IMapCollision;
+//import group8.common.services.IGamePluginService;
+//import java.util.ArrayList;
+//import org.openide.util.Lookup;
+//import org.openide.util.lookup.ServiceProvider;
+//import org.openide.util.lookup.ServiceProviders;
+//
+///**
+// *
+// * @author jacob
+// */
+//@ServiceProviders(value = {@ServiceProvider(service = IGamePluginService.class)})
+//public class CollisionPluginSystem implements IGamePluginService  {
+//    
+//    
+//    @Override
+//    public void start(GameData gameData, World world) {
+//       
+//        
+//    }
+//    
+//    @Override
+//    public void stop(GameData gameData, World world) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//    
+//}
