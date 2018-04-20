11cb4926f29b4e4a12a9116edb5a9094bc275d1c
Øllebrød <3>
2018-04-20 10:41:09 +0200

    Added DamagePart

---
+++ Common/src/main/java/group8/common/data/entityparts/DamagePart.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group8.common.data.entityparts;
+
+import group8.common.data.Entity;
+import group8.common.data.GameData;
+
+/**
+ *
+ * @author MER
+ */
+public class DamagePart implements EntityPart {
+    
+    private int damage;
+    
+    @Override
+    public void process(GameData gameData, Entity entity) {
+    }
+
+    /**
+     * @return the damage
+     */
+    public int getDamage() {
+        return damage;
+    }
+
+    /**
+     * @param damage the damage to set
+     */
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    
+    
+}
