27e99c45e1a3e2bb34adeb694c1a3d076dd6e739
Unknown <jacos16@student.sdu.dk>
2018-04-24 16:14:26 +0200

    Zombie can't spawn close to player but all is inside map

--- Zombie/src/main/java/group8/zombie/Zombie.java
+++ Zombie/src/main/java/group8/zombie/Zombie.java
@@ -7,11 +7,15 @@ package group8.zombie;
 
 import group8.common.data.Entity;
 import group8.common.data.EntityType;
+import group8.common.data.GameData;
+import group8.common.data.entityparts.PositionPart;
 import group8.common.playercommon.IPlayerService;
 import group8.commonenemy.enemy.Enemy;
 import group8.commonenemy.enemy.EntityDamage;
 import group8.commonenemy.enemy.Rating;
 import group8.commonenemy.services.IPathFinderService;
+import java.util.Random;
+import org.openide.util.Lookup;
 
 /**
  * Super class for zombies. Only sets the entity type to be a zombie.
@@ -19,12 +23,70 @@ import group8.commonenemy.services.IPathFinderService;
  */
 public abstract class Zombie extends Enemy {
     private EntityDamage damage;
+    private Random r = new Random();
 
     
     public Zombie(Rating rating) {
         super(rating);
 
         this.setType(EntityType.ZOMBIE);
+    }
+    
+    //Method to set the x value of a new zombie with defined distance from player
+    public float setX(GameData gameData, float distance){
+        float playerX = this.getPlayerPos().getX();
+        float x = r.nextInt((int) (((gameData.getDisplayWidth()) - (gameData.getDisplayWidth() / 2)) 
+                + playerX));
+        
+        //If zombie is too close to player, move it.
+        if (x >= playerX) {
+            x += distance;
+        } else {
+            x -= distance;
+        }
+        
+        //If zombie is over one of the edges move it inside
+        if (x < 40) {
+            x = 40;
+        }
+        
+        if (x > 1500) {
+            x = 1500;
+        }
+        
+        return x;
+    }
+    
+    //Method to set the y value of a new zombie with defined distance from player
+    public float setY(GameData gameData, float distance){
+         float playerY = this.getPlayerPos().getY();
+        
+        float y = r.nextInt((int) (((gameData.getDisplayHeight()) - (gameData.getDisplayHeight() / 2)) 
+                + playerY));
+        
+        //If zombie is too close to player, move it.
+        if (y >= playerY) {
+            y += distance;
+        } else {
+            y -= distance;
+        }
+        
+        //If zombie is over one of the edges move it inside
+        if (y < 40) {
+            y = 40;
+        }
+        
+        if (y > 1100) {
+            y = 1100;
+        }
+        return y;
+    }
+    
+    private PositionPart getPlayerPos(){
+        Lookup lookup = Lookup.getDefault();
+        IPlayerService ps = lookup.lookup(IPlayerService.class);
+        Entity player = ps.getPlayer();
+        return player.getPart(PositionPart.class);
     }
     
 }
