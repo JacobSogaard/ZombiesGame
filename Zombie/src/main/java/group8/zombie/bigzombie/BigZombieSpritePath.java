5d2fd4eddef62ad2d0659e3ad76eadfe3fbce4da
Unknown <Christensen.H.Kasper@gmail.com>
2018-04-24 15:06:53 +0200

    Added a new zombie image (8 stk.)

--- Zombie/src/main/java/group8/zombie/bigzombie/BigZombieSpritePath.java
+++ Zombie/src/main/java/group8/zombie/bigzombie/BigZombieSpritePath.java
@@ -10,12 +10,12 @@ package group8.zombie.bigzombie;
  * @author jacob
  */
 public class BigZombieSpritePath {
-    protected static final String UP = "Images/EnemyImages/UP.png"; 
-    protected static final String LEFT = "Images/EnemyImages/LEFT.png"; 
-    protected static final String DOWN = "Images/EnemyImages/DOWN.png"; 
-    protected static final String RIGHT = "Images/EnemyImages/RIGHT.png"; 
-    protected static final String UPLEFT = "Images/EnemyImages/UPLEFT.png"; 
-    protected static final String UPRIGHT = "Images/EnemyImages/UPRIGHT.png"; 
-    protected static final String DOWNLEFT = "Images/EnemyImages/DOWNLEFT.png";  
-    protected static final String DOWNRIGHT = "Images/EnemyImages/DOWNRIGHT.png"; 
+    protected static final String UP = "Images/EnemyImages/BigZombie/UP.png"; 
+    protected static final String LEFT = "Images/EnemyImages/BigZombie/LEFT.png"; 
+    protected static final String DOWN = "Images/EnemyImages/BigZombie/DOWN.png"; 
+    protected static final String RIGHT = "Images/EnemyImages/BigZombie/RIGHT.png"; 
+    protected static final String UPLEFT = "Images/EnemyImages/BigZombie/UPLEFT.png"; 
+    protected static final String UPRIGHT = "Images/EnemyImages/BigZombie/UPRIGHT.png"; 
+    protected static final String DOWNLEFT = "Images/EnemyImages/BigZombie/DOWNLEFT.png";  
+    protected static final String DOWNRIGHT = "Images/EnemyImages/BigZombie/DOWNRIGHT.png"; 
 }
--- Zombie/src/main/java/group8/zombie/smallzombie/SmallZombieSpritePath.java
+++ Zombie/src/main/java/group8/zombie/smallzombie/SmallZombieSpritePath.java
@@ -10,12 +10,12 @@ package group8.zombie.smallzombie;
  * @author jacob
  */
 public class SmallZombieSpritePath {
-    protected static final String UP = "Images/EnemyImages/UP.png"; 
-    protected static final String LEFT = "Images/EnemyImages/LEFT.png"; 
-    protected static final String DOWN = "Images/EnemyImages/DOWN.png"; 
-    protected static final String RIGHT = "Images/EnemyImages/RIGHT.png"; 
-    protected static final String UPLEFT = "Images/EnemyImages/UPLEFT.png"; 
-    protected static final String UPRIGHT = "Images/EnemyImages/UPRIGHT.png"; 
-    protected static final String DOWNLEFT = "Images/EnemyImages/DOWNLEFT.png";  
-    protected static final String DOWNRIGHT = "Images/EnemyImages/DOWNRIGHT.png"; 
+    protected static final String UP = "Images/EnemyImages/SmallZombie/UP.png"; 
+    protected static final String LEFT = "Images/EnemyImages/SmallZombie/LEFT.png"; 
+    protected static final String DOWN = "Images/EnemyImages/SmallZombie/DOWN.png"; 
+    protected static final String RIGHT = "Images/EnemyImages/SmallZombie/RIGHT.png"; 
+    protected static final String UPLEFT = "Images/EnemyImages/SmallZombie/UPLEFT.png"; 
+    protected static final String UPRIGHT = "Images/EnemyImages/SmallZombie/UPRIGHT.png"; 
+    protected static final String DOWNLEFT = "Images/EnemyImages/SmallZombie/DOWNLEFT.png";  
+    protected static final String DOWNRIGHT = "Images/EnemyImages/SmallZombie/DOWNRIGHT.png"; 
 }
